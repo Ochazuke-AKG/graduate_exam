9b81611feb586c10bff4ef93245780e7377b51d7
mokk <kommok03@gmail.com>
2026-01-28 00:02:46 +0900

    日記作成機能修正

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,3 +1,9 @@
 class Diary < ApplicationRecord
   belongs_to :user
+
+  validates :good_1, presence: true
+  validates :good_2, presence: true
+  validates :good_3, presence: true
+  validates :entry_date, presence: true
+
 end
